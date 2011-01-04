549e34d91fe5e2acc4f888ceae173e4d10b4f00f
Shashank Date <sdate@zavers.com>
2011-01-04 12:08:36 -0600

    Added a more comprehensive and realitic data file to read from

--- cron_timeline.rb
+++ cron_timeline.rb
@@ -4,7 +4,9 @@ require 'json'
 require 'models/parse_cron'
 
 get "/cron" do
-  cron_jobs = ['*/15  *  *  1 * whoami','*/30  *  *  1 * uname' ]
+  # cron_jobs = ['*/15  *  *  1 * whoami','*/30  *  *  1 * uname' ]
+  # cron_jobs = `crontab -l`
+  cron_jobs = File.readlines("cronfile.txt")
   hr_window = 2
   erb :cron, :locals => { :json => to_timeline_hash(cron_jobs,hr_window).to_json } 
 end 
