27c8a894f4f47f70ecece4636570d9daaf2f0c53
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-01-04 21:23:20 -0600

    Still doesn't seem to be loading the sample data, so changing to hardcoded data

--- cron_timeline.rb
+++ cron_timeline.rb
@@ -8,9 +8,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'models', 'parse_cron
 # end
 
 get "/cron" do
-  # cron_jobs = ['*/15  *  *  1 * whoami','*/30  *  *  1 * uname' ]
+  cron_jobs = ['*/15  *  *  1 * whoami','*/30  *  *  1 * uname' ]
   # cron_jobs = `crontab -l`
-  cron_jobs = File.readlines(File.expand_path(File.join(File.dirname(__FILE__), 'cronfile.txt')))
+  # cron_jobs = File.readlines(File.expand_path(File.join(File.dirname(__FILE__), 'cronfile.txt')))
   hr_window = 2
   erb :cron, :locals => { :json => to_timeline_hash(cron_jobs,hr_window).to_json } 
 end 
