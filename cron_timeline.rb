f9f8fc83039f6642d7f61ff9efaccea48a2dfd4c
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-01-04 21:21:03 -0600

    Fixed (hopefully) path to sample data, so it will load Removed handler for /

--- cron_timeline.rb
+++ cron_timeline.rb
@@ -3,14 +3,14 @@ require 'sinatra'
 require 'json'
 require File.expand_path(File.join(File.dirname(__FILE__), 'models', 'parse_cron')) 
 
-get "/" do
-  "Hello from Sinatra on Heroku!"
-end
+# get "/" do
+#   "Hello from Sinatra on Heroku!"
+# end
 
 get "/cron" do
   # cron_jobs = ['*/15  *  *  1 * whoami','*/30  *  *  1 * uname' ]
   # cron_jobs = `crontab -l`
-  cron_jobs = File.readlines("cronfile.txt")
+  cron_jobs = File.readlines(File.expand_path(File.join(File.dirname(__FILE__), 'cronfile.txt')))
   hr_window = 2
   erb :cron, :locals => { :json => to_timeline_hash(cron_jobs,hr_window).to_json } 
 end 
